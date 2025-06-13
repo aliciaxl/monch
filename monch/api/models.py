e91c344f4c28abbee52ba3d37cd8f92385814aed
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-06-13 16:27:46 -0700

    Add db and framework to settings.py

--- monch/api/models.py
+++ monch/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create models using Django ORM:
+
+class User(AbstractUser):
+    display_name = models.CharField(max_length=25, unique=True)
