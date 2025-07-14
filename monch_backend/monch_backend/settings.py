8dbf2ab325ecb713663c0fe47f09f97835cc5d1c
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-07-14 14:15:04 -0700

    fix wsgi.py

--- monch_backend/monch_backend/settings.py
+++ monch_backend/monch_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://monch-backend.onrender.com']
 
 # Application definition
 
--- monch_backend/monch_backend/wsgi.py
+++ monch_backend/monch_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'threads_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monch_backend.settings')
 
 application = get_wsgi_application()
