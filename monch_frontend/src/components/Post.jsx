3b5b5de6e4ea14ec4286f2fba5e88000928c99d7
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-07-14 17:47:21 -0700

    Change like button from rose to red

--- monch_frontend/src/components/Post.jsx
+++ monch_frontend/src/components/Post.jsx
@@ -205,7 +205,7 @@ export default function Post({
                 onClick={toggleLike}
                 disabled={loading}
                 className={`cursor-pointer flex items-center space-x-1 ${
-                  liked ? "text-rose-400" : "hover:text-white"
+                  liked ? "text-red-400" : "hover:text-white"
                 }`}
                 aria-label={liked ? "Unlike post" : "Like post"}
               >
