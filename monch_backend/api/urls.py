bf78a21cc2cc8a0fcf36ef672ad0f607029b494e
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-07-16 22:57:55 -0700

    Remove path

--- monch_backend/api/urls.py
+++ monch_backend/api/urls.py
@@ -12,7 +12,6 @@ router.register(r'follows', follow_views.FollowViewSet)
 router.register(r'likes', like_views.LikeViewSet)
 
 urlpatterns = [
-    path("health/", health),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
