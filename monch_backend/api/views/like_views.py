86e19470d66230bc0dc3016498294782b2d4e336
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-07-19 15:22:05 -0700

    Reorder created at

--- monch_backend/api/views/like_views.py
+++ monch_backend/api/views/like_views.py
@@ -12,7 +12,7 @@ class LikeViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'], url_path='liked-posts')
     def liked_posts(self, request):
         user = request.user
-        likes = Like.objects.filter(user=user).select_related('post').order_by('created_at')
+        likes = Like.objects.filter(user=user).select_related('post').order_by('-created_at')
         posts = [like.post for like in likes]
         serializer = PostSerializer(posts, many=True, context={'request': request})
         return Response(serializer.data)
